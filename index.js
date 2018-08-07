80aa927b9527dad05331e150cf6aa5ad2273cde0
SarahSpajic <sarahwagner1105@gmail.com>
2018-08-07 12:41:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ function logWhisper (string) {
 
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase) {
-    return "I can\'t hear you!"
+    return ("I can\'t hear you!")
   }
 }
 
 function sayHiToGrandma(string) {
   if (string === string.toUpperCase) {
-    return ('YES INDEED!')
+    return ("YES INDEED!")
   }
 }
 
